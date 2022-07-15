86367d107b3e3d36ace1b987a6e7fd82312bad82
Dorin Ilusca <dorin.ilusca@hotmail.com>
2022-07-16 00:25:35 +0300

    testing nprogeress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/global.css";
+import "../styles/nprogress.css";
+import NProgress from "../components/nprogress";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <NProgress />
+    </>
+  );
 }
